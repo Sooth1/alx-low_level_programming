1133ee6878808e453aedb507ef8c0f0500b66c67
Sooth1 <tgaebuelwe@yahoo.com>
2022-09-22 05:21:32 -0700

    2. strncpy

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 char *_strncat(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 
 #endif
